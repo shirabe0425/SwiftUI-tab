3264e0b5acc5de081a00578b8a2899cf124d08d5
shirabe0425 <84608407+shirabe0425@users.noreply.github.com>
2021-05-22 16:43:49 +0900

    first commit

---
+++ SwiftUI-tab/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SwiftUI-tab
+//
+//  Created by MacAir03 on 2021/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var selectedTag=1
+    var body: some View {
+        TabView(selection: $selectedTag){
+            VStack(){
+                Text("基本情報")
+                Text("名前：藤原詩")
+            }.tabItem { Text("基本情報") }.tag(1)
+            
+            Text("Tab Content 2").tabItem { Text("趣味") }.tag(2)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-tab/SwiftUI_tabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tabApp.swift
+//  SwiftUI-tab
+//
+//  Created by MacAir03 on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
